3be7996f86198b0afdad467716ca3d6e401ed461
Ícaro Magnago <icaro.magnago@anbima.com.br>
2020-10-25 12:42:08 -0300

    untracking file

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyAxkHHcuJNvSFHaOj_uMw0oJLDzY8oWCRU",
-    authDomain: "whatsappclone-f5f77.firebaseapp.com",
-    databaseURL: "https://whatsappclone-f5f77.firebaseio.com",
-    projectId: "whatsappclone-f5f77",
-    storageBucket: "whatsappclone-f5f77.appspot.com",
-    messagingSenderId: "688505358901",
-    appId: "1:688505358901:web:e9eb4ede75a6e6e982ff7c"
-};
