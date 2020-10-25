9dabe24be33c55b1077e942e4e429e476cd12bb5
Ícaro Magnago <icaro.magnago@anbima.com.br>
2020-10-25 12:46:07 -0300

    firebase config file example

---
+++ src/firebaseConfig-example.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "<api-key>",
+    authDomain: "<auth-domain>",
+    databaseURL: "<database-url>",
+    projectId: "<project-id>",
+    storageBucket: "<store-bucket>",
+    messagingSenderId: "<messaging-sender-id>",
+    appId: "<api-id>"
+};
