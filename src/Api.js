8b9f7e671c10ce31528e7837db0ade7d4bf82e67
Ícaro Magnago <icaro.magnago@anbima.com.br>
2020-10-25 12:32:31 -0300

    Whatsapp clone

---
+++ src/Api.js
@@ -0,0 +1,134 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/firebase-firestore';
+
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default {
+    fbPopup: async () => {
+        const provider = new firebase.auth.FacebookAuthProvider();
+        let result = await firebaseApp.auth().signInWithPopup(provider);
+
+        return result;
+    },
+    addUser: async (user) => {
+        await db.collection('users').doc(user.id).set({
+            name: user.name,
+            avatar: user.avatar
+        }, 
+        { 
+            merge: true 
+        });
+    },
+    getContactList: async (userId) => {
+        let list = [];
+
+        let results = await db.collection('users').get();
+        results.forEach(result => {
+            let data = result.data();
+            if (result.id !== userId) {
+                list.push({
+                    id: result.id,
+                    name: data.name,
+                    avatar: data.avatar
+                });
+            }
+        });
+
+        return list;
+    },
+    addNewChat: async (user, user2) => {
+        let newChat = await db.collection('chats').add({
+            messages: [],
+            users: [user.id, user2.id]
+        });
+
+        db.collection('users').doc(user.id).update({
+            chats: firebase.firestore.FieldValue.arrayUnion({
+                chatId: newChat.id,
+                title: user2.name,
+                image: user2.avatar,
+                with: user2.id
+            })
+        });
+
+        db.collection('users').doc(user2.id).update({
+            chats: firebase.firestore.FieldValue.arrayUnion({
+                chatId: newChat.id,
+                title: user.name,
+                image: user.avatar,
+                with: user.id
+            })
+        });
+    },
+    onChatList: (userId, setChatList) => {
+        return db.collection('users').doc(userId).onSnapshot((doc) => {
+            if (doc.exists) {
+                let data = doc.data();
+
+                if (data.chats) {
+                    let chats = [...data.chats];
+
+                    chats.sort((chatA, chatB) => {
+                        if (chatA.lastMessageDate === undefined) {
+                            return -1;
+                        }
+                        if (chatB.lastMessageDate === undefined) {
+                            return -1;
+                        }
+                        if (chatA.lastMessageDate.seconds < chatB.lastMessageDate.seconds) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
+                    });
+
+                    setChatList(chats);
+                }
+            }
+        });
+    },
+    onChatContent: (chatId, setList, setUsers) => {
+        return db.collection('chats').doc(chatId).onSnapshot((doc) => {
+            if (doc.exists) {
+                let data = doc.data();
+                setList(data.messages);
+                setUsers(data.users);
+            }
+        })
+    },
+    sendMessage: async (chatData, userId, type, body, users) => {
+        let now = new Date();
+
+        db.collection('chats').doc(chatData.chatId).update({
+            messages: firebase.firestore.FieldValue.arrayUnion({
+                type,
+                author: userId,
+                body,
+                date: now
+            })
+        });
+
+        users && users.forEach(async u => {
+            let user = await db.collection('users').doc(u).get();
+            let userData = user.data();
+            if (userData.chats) {
+                let chats = [...userData.chats];
+                
+                chats
+                    .filter(chat => chat.chatId === chatData.chatId)
+                    .forEach(chat => {
+                        chat.lastMessage = body;
+                        chat.lastMessageDate = now;
+                    });
+                
+                    await db.collection('users').doc(u).update({
+                        chats
+                    });
+            }
+        })
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Api from './Api';
 
-export default App;
+import ChatListItem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro';
+import ChatWindow from './components/ChatWindow';
+import NewChat from './components/NewChat';
+import Login from './components/Login';
+
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
+
+  const [chatlist, setChatList] = useState([]);
+  const [activeChat, setActiveChat] = useState({});
+  const [user, setUser] = useState(null);
+  const [showNewChat, setShowNewChat] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      let unsub = Api.onChatList(user.id, setChatList);
+      
+      return unsub;
+    }
+  }, [user]);
+
+  const handleNewChat = () => {
+    setShowNewChat(true);
+  }
+
+  const handleLoginData = async (facebookUser) => {
+    let newUser = {
+      id: facebookUser.uid,
+      name: facebookUser.displayName,
+      avatar: facebookUser.photoURL
+    };
+
+    await Api.addUser(newUser);
+
+    setUser(newUser);
+  }
+
+  if (user === null) {
+    return (<Login onReceive={handleLoginData} />);
+  }
+
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+        <NewChat
+          chatlist={chatlist}
+          user={user} 
+          show={showNewChat} 
+          setShow={setShowNewChat} />
+        <header>
+          <img 
+            className="header--avatar" 
+            alt="" 
+            src={user.avatar} />
+
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon onClick={handleNewChat} style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}} />
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon fontSize="small" style={{color: '#919191'}} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+          </div>
+        </div>
+
+        <div className="chatlist">
+          {chatlist.map((item, key) => (
+            <ChatListItem 
+              key={key}
+              data={item}
+              active={activeChat.chatId === chatlist[key].chatId}
+              onClick={() => setActiveChat(chatlist[key])} 
+            />
+          ))}
+        </div>
+        
+      </div>
+      <div className="contentarea">
+        {activeChat.chatId ? (
+          <ChatWindow 
+            user={user} 
+            data={activeChat} />
+        ) : (
+          <ChatIntro />
+        )}
+        
+      </div>
+    </div>
+  )
+}
